9cffda83b545a6be6d38511839c7a8014656ea8f
briandaniel14 <briandaniel789@gmail.com>
2026-09-03 17:31:26 +0200

    Adding tests

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello world!");
 }
+<<<<<<< HEAD
+=======
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn basic_test() {
+        assert_eq!(2 + 2, 4)
+    }
+}
+>>>>>>> 31fee0e (Initial commit)
